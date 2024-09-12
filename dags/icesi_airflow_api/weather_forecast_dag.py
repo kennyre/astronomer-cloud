e60044e9c313683fa28a4b9abe09ab898860794c
cristianurcuqui <cristian.urcuqui@rappicard.mx>
2024-09-11 22:42:38 -0500

    Add weather forecast query and Snowflake integration

--- dags/icesi_airflow_api/weather_forecast_dag.py
+++ dags/icesi_airflow_api/weather_forecast_dag.py
@@ -1,14 +1,21 @@
+import os
 from airflow import DAG
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
-import logging
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator  # Import del operador de Snowflake
 
+import logging
 from dags.icesi_airflow_api.utils.weather_api import run_weather_forecast_pipeline
 
 # Parámetros de configuración
 URL = 'https://smn.conagua.gob.mx/tools/GUI/webservices/index.php?method=3'
 AWS_CONN_ID = 'aws_conn_s3'
 S3_BUCKET = 'api-airflow'
+SNOWFLAKE_CONN_ID = 'snowflake_conn_id'
+QUERIES_BASE_PATH = os.path.join(os.path.dirname(__file__), 'queries')
+
+# Definir una variable universal para la fecha de ejecución
+date_str = '{{ ds }}'  # Plantilla Jinja para obtener la fecha de ejecución en formato 'YYYY-MM-DD'
 
 # Definimos el DAG usando el decorador @dag
 @dag(
@@ -17,14 +24,13 @@ S3_BUCKET = 'api-airflow'
     catchup=False,               # Evitar la ejecución de tareas atrasadas
     default_args={'owner': 'airflow', 'retries': 1},
     tags=['weather', 's3'],      # Etiquetas para categorizar el DAG
+    template_searchpath=QUERIES_BASE_PATH
 )
 def weather_forecast_dag():
     
     # Definimos una tarea con el decorador @task
     @task()
-    def fetch_and_save_weather_data(execution_date=None):
-        # Utilizamos la fecha de ejecución ({{ ds }}) para crear un archivo único cada día
-        date_str = execution_date.strftime('%Y-%m-%d')
+    def fetch_and_save_weather_data():
         s3_key_with_date = f'weather_data/weather_data_{date_str}.csv'
         
         # Ejecutar el pipeline con el nuevo nombre de archivo
@@ -34,9 +40,19 @@ def weather_forecast_dag():
             s3_bucket=S3_BUCKET,
             s3_key=s3_key_with_date
         )
+
+    # Tarea para ejecutar una query en Snowflake usando el SnowflakeOperator
+    execute_snowflake_query = SnowflakeOperator(
+        task_id='execute_snowflake_query',
+        snowflake_conn_id=SNOWFLAKE_CONN_ID,
+        sql='weather_forecast_query.sql',  # Ruta del archivo SQL dentro del template_searchpath
+        params={
+            'dt': date_str  # Parametro para la query
+        }
+    )
     
-    # Llamamos a la tarea para crear su dependencia
-    fetch_and_save_weather_data()
+    # Definir dependencias entre las tareas
+    fetch_and_save_weather_data() >> execute_snowflake_query
 
 # Instancia del DAG
 dag = weather_forecast_dag()
