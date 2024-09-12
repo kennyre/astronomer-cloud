4ddf4d0f265543387050fa905d34391a06914ca2
cristianurcuqui <cristian.urcuqui@rappicard.mx>
2024-09-11 22:55:50 -0500

    Update S3 bucket path for weather data in weather_forecast_query.sql

--- dags/icesi_airflow_api/queries/weather_forecast_query.sql
+++ dags/icesi_airflow_api/queries/weather_forecast_query.sql
@@ -1,4 +1,4 @@
 COPY INTO RAW_DATA.CONAGUA_WEATHER.weather_forecast_raw
-FROM 's3://{S3_BUCKET}/weather_data/{{params.dt}}.csv'
+FROM '@stage_raw_data/weather_data/{{params.dt}}.csv'
 FILE_FORMAT = (TYPE = 'CSV', FIELD_OPTIONALLY_ENCLOSED_BY = '"', SKIP_HEADER = 1);
         
