88aa744f802e735a05c409033b74d903cd0355d7
cristianurcuqui <cristian.urcuqui@rappicard.mx>
2024-09-11 23:06:41 -0500

    Update weather forecast query file path

--- dags/icesi_airflow_api/queries/weather_forecast_query.sql
+++ dags/icesi_airflow_api/queries/weather_forecast_query.sql
@@ -1,4 +1,4 @@
 COPY INTO RAW_DATA.CONAGUA_WEATHER.weather_forecast_raw
-FROM '@RAW_DATA.CONAGUA_WEATHER.stage_raw_data/weather_data/{{params.dt}}.csv'
+FROM '@RAW_DATA.CONAGUA_WEATHER.stage_raw_data/weather_data/weather_data_{{params.dt}}.csv'
 FILE_FORMAT = (TYPE = 'CSV', FIELD_OPTIONALLY_ENCLOSED_BY = '"', SKIP_HEADER = 1);
         
